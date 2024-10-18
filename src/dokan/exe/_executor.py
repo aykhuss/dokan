45bcad3ec580e50da5d75e16ee1ab426661ff3d0
Alexander Huss <alexander.huss@cern.ch>
2024-10-18 18:47:02 +0200

    write log-file parser to extract job data. update executor to populate ExeData information.

--- src/dokan/exe/_executor.py
+++ src/dokan/exe/_executor.py
@@ -9,12 +9,12 @@ import logging
 
 from abc import ABCMeta, abstractmethod
 from pathlib import Path
-from os import PathLike
+import os
 import time
 
 from ._exe_config import ExecutionMode, ExecutionPolicy
 from ._exe_data import ExeData
-import dokan.runcard
+from ..nnlojet import parse_log_file
 
 logger = logging.getLogger("luigi-interface")
 
@@ -24,7 +24,7 @@ class Executor(luigi.Task, metaclass=ABCMeta):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.exe_data: ExeData = ExeData(Path(self.path), expect_tmp=True)
+        self.exe_data: ExeData = ExeData(Path(self.path))
 
     @staticmethod
     def factory(policy=ExecutionPolicy.LOCAL, *args, **kwargs):
@@ -48,70 +48,46 @@ class Executor(luigi.Task, metaclass=ABCMeta):
         pass
 
     def run(self):
-        print(f"Executor: run {self.path}")
-        self.exe_data["timestamp"] = time.time()
+        print(f"[{time.time()}] Executor: run {self.path}")
+
         # > more preparation for execution?
+
         # @todo check if job files are already there? (recovery mode?)
+
         self.exe()
-        # > exe done, generate the output file.
-        self.exe_data["timestamp"] = time.time()
-        # @todo: loop over all generated files, parse log files to populate results and errors
-        print(f"Executor: finalize {self.path}")
+        print(f"[{time.time()}] Executor: done with exe {self.path}")
+
+        # > exe done populate job data and write target file
+
+        # > keep track of files that were generated
+        for entry in os.scandir(self.path):
+            if entry.name in self.exe_data["input_files"]:
+                continue
+            if entry.name in [ExeData._file_tmp, ExeData._file_fin]:
+                continue
+            print(f"> [{entry.stat().st_mtime} < {self.exe_data["timestamp"]} {entry.stat().st_mtime < self.exe_data["timestamp"]}] {self.path}: {entry.name}")
+            if entry.stat().st_mtime < self.exe_data["timestamp"]:
+                continue
+            # > genuine output file that was generated/modified
+            self.exe_data["output_files"].append(entry.name)
+
+        print(f"  >>  {self.exe_data["output_files"]}:")
+
+        for job_id, job_data in self.exe_data["jobs"].items():
+            print(f".s{job_data["seed"]}.log")
+            log_matches = [
+                of
+                for of in self.exe_data["output_files"]
+                if of.endswith(f".s{job_data["seed"]}.log")
+            ]
+            print(f" > log_matches: {log_matches}")
+            if len(log_matches) != 1:
+                continue
+            parsed_data = parse_log_file(Path(self.path) / log_matches[0])
+            print(f" > parsed data: {parsed_data}")
+            for key in parsed_data:
+                job_data[key] = parsed_data[key]
+
+        print(f"[{time.time()}] Executor: finalize {self.path}")
+        # > add last modification time?
         self.exe_data.finalize()
-
-        # # > parse the output file to extract some information
-        # with open(self._local(Executor._file_out), "r") as of:
-        #     iteration = {}
-        #     for line in of:
-        #         match_iteration = re.search(
-        #             r"\(\s*iteration\s+(\d+)\s*\)", line, re.IGNORECASE
-        #         )
-        #         if match_iteration:
-        #             iteration["nit"] = int(match_iteration.group(1))
-        #         match_integral = re.search(
-        #             r"\bintegral\s*=\s*(\S+)\s+accum\.\s+integral\s*=\s*(\S+)\b",
-        #             line,
-        #             re.IGNORECASE,
-        #         )
-        #         if match_integral:
-        #             iteration["val"] = float(match_integral.group(1))
-        #             iteration["acc_val"] = float(match_integral.group(2))
-        #         match_stddev = re.search(
-        #             r"\bstd\.\s+dev\.\s*=\s*(\S+)\s+accum\.\s+std\.\s+dev\s*=\s*(\S+)\b",
-        #             line,
-        #             re.IGNORECASE,
-        #         )
-        #         if match_stddev:
-        #             iteration["err"] = float(match_stddev.group(1))
-        #             iteration["acc_err"] = float(match_stddev.group(2))
-        #         match_chi2it = re.search(
-        #             r"\schi\*\*2/iteration\s*=\s*(\S+)\b", line, re.IGNORECASE
-        #         )
-        #         if match_chi2it:
-        #             iteration["chi"] = float(match_chi2it.group(1))
-        #             self.data["iterations"].append(iteration)
-        #             iteration = {}
-
-        # # > save the accumulated result
-        # self.data["integral"] = self.data["iterations"][-1]["acc_val"]
-        # self.data["error"] = self.data["iterations"][-1]["acc_err"]
-        # self.data["chi2dof"] = self.data["iterations"][-1]["chi"]
-
-        # # > keep track of files that were generated
-        # # > new recommendation is to use os.scandir()
-        # for file in os.listdir(self._path):
-        #     if file in [
-        #         Executor._file_run,
-        #         Executor._file_res,
-        #         Executor._file_tmp,
-        #         Executor._file_out,
-        #         Executor._file_err,
-        #     ]:
-        #         continue
-        #     if os.stat(self._local(file)).st_mtime < start_time:
-        #         continue
-        #     # > genuine output file that was generated/modified
-        #     self.data["output_files"].append(file)
-
-        # with open(self._local(Executor._file_tmp), "w") as outfile:
-        #     json.dump(self.data, outfile, indent=2)
--- src/dokan/nnlojet.py
+++ src/dokan/nnlojet.py
@@ -7,8 +7,8 @@ import re
 import subprocess
 from os import PathLike
 
-from .order import Order
 
+from .order import Order
 
 def get_lumi(exe: PathLike, proc: str) -> dict:
     """get channels for an NNLOJET process
@@ -68,11 +68,65 @@ def get_lumi(exe: PathLike, proc: str) -> dict:
     return chan_list
 
 
-#@todo
-def parse_log_file(log_file: PathLike) -> list[dict[str, float]]:
-    return [{"a": 1.0}]
+def parse_log_file(log_file: PathLike) -> dict:
+    # > return value follows the structure of ExeData["jobs"][<id>]["iterations"]
+    job_data: dict = {}
+    job_data["iterations"] = []
+    # > parse the output file to extract some information
+    with open(log_file, "r") as lf:
+        iteration = {}
+        for line in lf:
+            match_iteration = re.search(
+                r"\(\s*iteration\s+(\d+)\s*\)", line, re.IGNORECASE
+            )
+            if match_iteration:
+                iteration["iteration"] = int(match_iteration.group(1))
+            match_integral = re.search(
+                r"\bintegral\s*=\s*(\S+)\s+accum\.\s+integral\s*=\s*(\S+)\b",
+                line,
+                re.IGNORECASE,
+            )
+            if match_integral:
+                iteration["result"] = float(match_integral.group(1))
+                iteration["result_acc"] = float(match_integral.group(2))
+            match_stddev = re.search(
+                r"\bstd\.\s+dev\.\s*=\s*(\S+)\s+accum\.\s+std\.\s+dev\s*=\s*(\S+)\b",
+                line,
+                re.IGNORECASE,
+            )
+            if match_stddev:
+                iteration["error"] = float(match_stddev.group(1))
+                iteration["error_acc"] = float(match_stddev.group(2))
+            match_chi2it = re.search(
+                r"\schi\*\*2/iteration\s*=\s*(\S+)\b", line, re.IGNORECASE
+            )
+            if match_chi2it:
+                iteration["chi2dof"] = float(match_chi2it.group(1))
+                job_data["iterations"].append(iteration)
+                iteration = {}
+            match_elapsed_time = re.search(
+                r"\s*Elapsed\s+time\s*=\s*(\S+)\b\s*(\S+)\b", line, re.IGNORECASE
+            )
+            if match_elapsed_time:
+                unit_time: str = match_elapsed_time.group(2)
+                fac_time: float = 1.0
+                if unit_time == "seconds":
+                    fac_time = 1.0
+                elif unit_time == "minutes":
+                    fac_time = 60.0
+                elif unit_time == "hours":
+                    fac_time = 3600.0
+                else:
+                    raise RuntimeError("unknown time unit")
+                job_data["elapsed_time"] = fac_time * float(match_elapsed_time.group(1))
+                #> the accumulated results
+                job_data["result"] = job_data["iterations"][-1]["result_acc"]
+                job_data["error"] = job_data["iterations"][-1]["error_acc"]
+                job_data["chi2dof"] = job_data["iterations"][-1]["chi2dof"]
+
+    return job_data
 
 
-#@ todo
+# @ todo
 def grid_score(grid_file: PathLike) -> float:
     return 42.0
