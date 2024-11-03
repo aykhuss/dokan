ac6e0a17793d257dbce0cb5346d08dba14eccb5c
Alexander Huss <alexander.huss@cern.ch>
2024-11-03 15:00:37 +0100

    switch to our own logging system in `Entry` task and send a kill signal to terminate the monitor process.

--- src/dokan/entry.py
+++ src/dokan/entry.py
@@ -4,6 +4,8 @@ import json
 
 from sqlalchemy import select
 
+from dokan.db._loglevel import LogLevel
+
 from .order import Order
 from .db import Part, Job, DBTask, DBInit, MergeAll
 from .db._dbtask import DBDispatch
@@ -18,9 +20,9 @@ class Entry(DBTask):
         return []
 
     def complete(self) -> bool:
-        njobs_rem, T_rem = self.remainders()
-        if njobs_rem <= 0 or T_rem <= 0.0:
-            return True
+        # njobs_rem, T_rem = self.remainders()
+        # if njobs_rem <= 0 or T_rem <= 0.0:
+        #     return True
         # @todo still need to check target_acc somehow.
         return False
 
@@ -30,7 +32,7 @@ class Entry(DBTask):
         preprods: list[PreProduction] = []
         with self.session as session:
             for pt in session.scalars(select(Part).where(Part.active.is_(True))):
-                self.logger(str(pt))
+                #self.debug(str(pt))
                 preprod = self.clone(
                     cls=PreProduction,
                     part_id=pt.id,
@@ -43,8 +45,8 @@ class Entry(DBTask):
         self.logger("Entry: complete MergeAll -> distribute time")
         opt_dist = self.distribute_time(100.0)
         for pt in sorted(opt_dist["part"].items(), key=lambda x: x[1]["T_opt"], reverse=True):
-            self.logger(f"{pt[0]}: {pt[1]}")
-        self.logger(
+            self.debug(f"{pt[0]}: {pt[1]}")
+        self.debug(
             f"estimate = {opt_dist["tot_result"]} +/- {opt_dist["tot_error_estimate_opt"]} [{100.*opt_dist["tot_error_estimate_opt"]/opt_dist["tot_result"]}%]"
         )
         # self.print_job()
@@ -55,3 +57,4 @@ class Entry(DBTask):
         yield dispatch
         self.logger("Entry: complete dispatch -> run MergeAll")
         yield self.clone(MergeAll, force=True)  # , run_tag=0.0)
+        self.logger("Entry: done", level=LogLevel.SIG_TERM)
